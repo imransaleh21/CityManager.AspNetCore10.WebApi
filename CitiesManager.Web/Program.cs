e718211f622ffefc595f37916ba2c5786a61faf8
Imran Saleh <126408093+imransaleh21@users.noreply.github.com>
2026-01-25 23:25:56 +0600

    Enable swagger

--- CitiesManager.Web/Program.cs
+++ CitiesManager.Web/Program.cs
@@ -10,12 +10,18 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
     );
 
+builder.Services.AddEndpointsApiExplorer(); // Generate OpenAPI/Swagger documents for API endpoints
+builder.Services.AddSwaggerGen(); // Add Swagger generator services
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHsts();
 app.UseHttpsRedirection();
+
+app.UseSwagger(); // Enable middleware to serve generated Swagger as a JSON endpoint.
+app.UseSwaggerUI(); // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
 
 app.UseAuthorization();
 
